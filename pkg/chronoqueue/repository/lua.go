b098389a0a29f63e2428675463799225eeb732dc
Adrien <n.adrien@mun.ca>
2023-04-06 15:14:28 -0400

    fix(lua): adds fix to lua script

--- pkg/chronoqueue/repository/lua.go
+++ pkg/chronoqueue/repository/lua.go
@@ -10,30 +10,25 @@ local current_time = tonumber(ARGV[1])
 local keys = redis.call('keys', '*')
 
 for _, key in ipairs(keys) do
-	redis.log(redis.LOG_NOTICE, "This is the key ===> ", key)
 	local key_type = redis.call('TYPE', key)["ok"]
-
-	redis.log(redis.LOG_NOTICE, "This is the keyType ===> ", key_type)
-
 	if key_type == 'hash' then
-		local invisibilityDuration = redis.call('hget', key, 'invisibilityDuration')
-		
+		local metadata = redis.call('hget', key, 'metadata')
+		local metadataJson = cjson.decode(metadata)
+		local invisibilityDuration = metadataJson["invisibilityDuration"]
+
 		if invisibilityDuration ~= nil then
 			invisibilityDuration = tonumber(invisibilityDuration)
-			redis.log(redis.LOG_NOTICE, "invisibilityDuration ===> ", invisibilityDuration)
-
 			if invisibilityDuration < current_time then
-				local fieldname = 'state'
-				local state = redis.call('HGET', key, fieldname)
-				if state ~= nil and state == '1' then
-					redis.log(redis.LOG_NOTICE, "Setting state to ===> PENDING for Key: ", key)
-					redis.call('hset', key, 'state', '2')
+				local state = metadataJson["state"]
+				if state ~= nil and state == "INVISIBLE" then
+					metadataJson["state"] = 'PENDING'
+					local updatedMetadata = cjson.encode(metadataJson)
+					redis.call('hset', key, 'metadata', updatedMetadata)
+					redis.log(redis.LOG_NOTICE, "Message with key", key, "updated to PENDING")
 					redis.log(redis.LOG_NOTICE, "===== DONE ====")
 				end
 			end
 		end
-	else
-		redis.log(redis.LOG_NOTICE, "Key is not of type HASH")
 	end
 end
 `)
