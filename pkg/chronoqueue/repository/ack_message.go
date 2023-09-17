37bb545efc5420f7b6685bcd604c767ea1c85f6b
Adrien <n.adrien@mun.ca>
2023-09-17 09:40:10 -0400

    feat(repository): adds validating state transitions allowed

--- pkg/chronoqueue/repository/ack_message.go
+++ pkg/chronoqueue/repository/ack_message.go
@@ -11,6 +11,18 @@ import (
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
+type transitionState int32
+
+const (
+	UNDEFINED transitionState = iota
+	INVISIBLE
+	PENDING
+	RUNNING
+	COMPLETED
+	CANCELED
+	ERRORED
+)
+
 // Updates and saves the message metadata in Redis.
 func (as *storage) saveMessageMetadata(ctx context.Context, queueName string, messageID string, metadata *chronoqueue.Message_Metadata) error {
 	m := protojson.MarshalOptions{
@@ -24,6 +36,23 @@ func (as *storage) saveMessageMetadata(ctx context.Context, queueName string, me
 	key := fmt.Sprintf("%s:%s:meta", queueName, messageID)
 	_, err = as.redisClient.HSet(ctx, key, "metadata", string(messageMetadataByte)).Result()
 	return err
+}
+
+// IsValidTransition checks if transitioning from the current state to a new state is valid based on the defined rules.
+func isValidTransition(currentState, newState transitionState) bool {
+	switch currentState {
+	case INVISIBLE:
+		return newState == PENDING
+	case PENDING:
+		return newState == RUNNING || newState == CANCELED
+	case RUNNING:
+		return newState == PENDING || newState == COMPLETED || newState == CANCELED || newState == ERRORED
+	case COMPLETED, CANCELED, ERRORED:
+		return false
+	default:
+		// For UNDEFINED or any other state not explicitly handled
+		return false
+	}
 }
 
 func (as *storage) AcknowledgeMessage(ctx context.Context, request *chronoqueue.AcknowledgeMessageRequest) (*chronoqueue.AcknowledgeMessageResponse, error) {
@@ -43,6 +72,12 @@ func (as *storage) AcknowledgeMessage(ctx context.Context, request *chronoqueue.
 		return nil, chronoErr.GRPCStatus()
 	}
 
+	// Check if the state transition is allowed
+	if !isValidTransition(transitionState(metadata.State), transitionState(request.State)) {
+		err := errors.New("invalid input: requested state transition is not allowed")
+		chronoErr := util.NewChronoError(util.ERROR_LEVEL_ERROR, codes.Internal, err, "Unexpected error occured while changing message state")
+		return nil, chronoErr.GRPCStatus()
+	}
 	// Update the message state
 	metadata.State = request.State
 
